5ff5cbe0db2c2a046f69fce0f30f344f43d6553d
kelvinokoh <114640434+kelvinokoh@users.noreply.github.com>
2022-10-01 02:48:10 -0400

    Add files via upload
    Baby Weather App: Keywords are ( rain, cloudy, windy, thunderstorm)

---
+++ Baby Weather App.py
@@ -0,0 +1,23 @@
+#BABY WEATHER APP
+# if its raining grab umbrella Otherwisw grab sunglasses
+#Boolean( True or False)
+#comparison operators( ==, <, >, <=, >=)
+
+weather = input('How is the weather today? ')
+if weather == 'rain':
+    print('Grab Umbrella')
+    print('Make sure to keep dry')
+elif weather == 'cloudy':
+    print('Jacket')
+    print('Might be a cool day')
+elif weather == 'windy':
+    print('Windbreaker')
+    print('Do not get blown away')
+elif weather == "thunderstorm":
+    print('Take cover')
+    print('Lightning incoming')
+else:
+    print('Sunglasses')
+    print('Might be a good day for a swim')
+
+

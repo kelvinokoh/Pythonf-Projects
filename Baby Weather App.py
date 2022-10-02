de8fdc033f7469940b294c7af17c79cad8fde686
kelvinokoh <114640434+kelvinokoh@users.noreply.github.com>
2022-10-01 23:10:40 -0400

    Update Baby Weather App.py

--- Baby Weather App.py
+++ Baby Weather App.py
@@ -3,21 +3,43 @@
 #Boolean( True or False)
 #comparison operators( ==, <, >, <=, >=)
 
-weather = input('How is the weather today? ')
-if weather == 'rain':
-    print('Grab Umbrella')
-    print('Make sure to keep dry')
-elif weather == 'cloudy':
-    print('Jacket')
-    print('Might be a cool day')
-elif weather == 'windy':
-    print('Windbreaker')
-    print('Do not get blown away')
-elif weather == "thunderstorm":
-    print('Take cover')
-    print('Lightning incoming')
-else:
-    print('Sunglasses')
-    print('Might be a good day for a swim')
+# weather = input('How is the weather today? ')
+# if weather == 'rain':
+#     print('Grab Umbrella')
+#     print('Make sure to keep dry')
+# elif weather == 'cloudy':
+#     print('Jacket')
+#     print('Might be a cool day')
+# elif weather == 'windy':
+#     print('Windbreaker')
+#     print('Do not get blown away')
+# elif weather == "thunderstorm":
+#     print('Take cover')
+#     print('Lightning incoming')
+# else:
+#     print('Sunglasses')
+#     print('Might be a good day for a swim')
+   
+# Using Defining Functions
+
+def weathertowarning(weather):
+    if weather == 'rain':
+        print('Grab Umbrella')
+        print('Make sure to keep dry')
+    elif weather == 'cloudy':
+        print('Jacket')
+        print('Might be a cool day')
+    elif weather == 'windy':
+        print('Windbreaker')
+        print('Do not get blown away')
+    elif weather == "thunderstorm":
+        print('Take cover')
+        print('Lightning incoming')
+    else:
+        print('Sunglasses')
+        print('Might be a good day for a swim')
+
+
+weathertowarning('cloudy')
 
 

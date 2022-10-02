dbe06d6a860889bc4ee143b964da1d70e01d7f9a
kelvinokoh <114640434+kelvinokoh@users.noreply.github.com>
2022-10-01 22:58:19 -0400

    Update Tip Calculator.py

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -13,16 +13,32 @@
 #print('your total is: '+ str(int(num1) + int(num2)))
 
 # Tip Calculator
-#math operators: +, -, /, *, **
-Food_amount = float(input('Enter food amount $: '))
-Tip_percent = float(input('Enter tip percentage %: ')) / 100
-Tip_amount = Food_amount * Tip_percent
-Total_paid = Tip_amount + Food_amount
-print('----------------------------')
-print(f'Food Amount:${Food_amount}')
-print(f'Tip Amount:${Tip_amount}')
-print('\n')
-print(f'Total Amoutn:${Total_paid}')
-print('----------------------------')
+# math operators:( +, -, /, *, ** )
+
+# Food_amount = float(input('Enter food amount $: '))
+# Tip_percent = float(input('Enter tip percentage %: ')) / 100
+# Tip_amount = Food_amount * Tip_percent
+# Total_paid = Tip_amount + Food_amount
+# print('----------------------------')
+# print(f'Food Amount:${Food_amount}')
+# print(f'Tip Amount:${Tip_amount}')
+# print('\n')
+# print(f'Total Amoutn:${Total_paid}')
+# print('----------------------------')
+
+def calculatefoodtotal(food, tip_percent):
+    tip = food * (tip_percent / 100)
+    total = tip + food
+    print('----------------------------')
+    print(f'Food Amount:${food}')
+    print(f'Tip Amount:${tip}')
+    print('\n')
+    print(f'Total Amoutn:${total}')
+    print('----------------------------')
+    return total
+
+
+# print(calculatefoodtotal(100, 10))
+calculatefoodtotal(food=100, tip_percent=10)
 
 
